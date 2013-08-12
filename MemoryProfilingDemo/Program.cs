a5b7444172a03812196f4730b4080fc6ab23fbb4
Ihor Bats <batsihor@gmail.com>
2013-08-12 12:51:40 +0300

    sample updated.

---
+++ MemoryProfilingDemo/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Harness
+{
+    static void Main()
+    {
+        Console.ReadLine(); // wait until profiler attaches
+        TestAsync().Wait();
+    }
+
+    static async Task TestAsync()
+    {
+#if true
+        var token = CancellationToken.None;
+        for (int i = 0; i < 100000; i++)
+        {
+#if true
+            await Task.FromResult(42).WithCancellation1(token);
+#else
+            await Task.FromResult(42).WithCancellation2(token);
+#endif
+        }
+#else
+        var token = new CancellationTokenSource().Token;
+        for (int i = 0; i < 100000; i++)
+        {
+            var tcs = new TaskCompletionSource<int>();
+            var t = tcs.Task.WithCancellation2(token);
+            tcs.SetResult(42);
+            await t;
+        }
+#endif
+    }
+}
+
+static class Extensions
+{
+    public static Task<T> WithCancellation2<T>(
+        this Task<T> task, CancellationToken cancellationToken)
+    {
+        if (task.IsCompleted || !cancellationToken.CanBeCanceled)
+            return task;
+        else if (cancellationToken.IsCancellationRequested)
+            return new Task<T>(() => default(T), cancellationToken);
+        else
+            return task.WithCancellation1(cancellationToken);
+    }
+
+    public static async Task<T> WithCancellation1<T>(
+        this Task<T> task, CancellationToken cancellationToken)
+    {
+        var tcs = new TaskCompletionSource<bool>();
+#if true
+        using (cancellationToken.Register(() => tcs.TrySetResult(true)))
+#else
+        using (cancellationToken.Register(s_cancellationRegistration, tcs))
+#endif
+            if (task != await Task.WhenAny(task, tcs.Task))
+                throw new OperationCanceledException(cancellationToken);
+        return await task;
+    }
+
+    private static readonly Action<object> s_cancellationRegistration =
+        s => ((TaskCompletionSource<bool>)s).TrySetResult(true);
+}
--- ObjectSizeDemo/Program.cs
+++ ObjectSizeDemo/Program.cs
@@ -6,10 +6,7 @@ namespace ObjectSizeDemo
     {
         private static void Main(string[] args)
         {
-            EmptyWithMethods obj = new EmptyWithMethods();
-
             ObjectSizes();
-
             OtherSizes();
 
             Console.ReadLine();
@@ -21,11 +18,20 @@ namespace ObjectSizeDemo
             long diff;
 
             long before = GC.GetTotalMemory(true);
-            Mixed2 obj = new Mixed2();
+            Empty obj = new Empty();
             long after = GC.GetTotalMemory(true);
             diff = after - before;
 
-            Console.WriteLine("Mixed2: \t\t" + diff);
+            Console.WriteLine("Empty: \t\t" + diff);
+
+            //---------------------------------------
+
+            before = GC.GetTotalMemory(true);
+            EmptyWithMethods obj0 = new EmptyWithMethods();
+            after = GC.GetTotalMemory(true);
+            diff = after - before;
+
+            Console.WriteLine("EmptyWithMethods: " + diff);
 
             //---------------------------------------
 
@@ -37,7 +43,7 @@ namespace ObjectSizeDemo
             Console.WriteLine("OneInt32: \t" + diff);
 
             //---------------------------------------
-            
+
             before = GC.GetTotalMemory(true);
             TwoInt32 obj2 = new TwoInt32();
             after = GC.GetTotalMemory(true);
@@ -46,7 +52,7 @@ namespace ObjectSizeDemo
             Console.WriteLine("TwoInt32: \t" + diff);
 
             //---------------------------------------
-            
+
             before = GC.GetTotalMemory(true);
             ThreeInt32 obj3 = new ThreeInt32();
             after = GC.GetTotalMemory(true);
@@ -55,7 +61,7 @@ namespace ObjectSizeDemo
             Console.WriteLine("ThreeInt32: \t" + diff);
 
             //---------------------------------------
-            
+
             before = GC.GetTotalMemory(true);
             Mixed1 obj4 = new Mixed1();
             after = GC.GetTotalMemory(true);
@@ -64,7 +70,7 @@ namespace ObjectSizeDemo
             Console.WriteLine("Mixed1: \t" + diff);
 
             //---------------------------------------
-            
+
             before = GC.GetTotalMemory(true);
             Mixed2 obj5 = new Mixed2();
             after = GC.GetTotalMemory(true);
@@ -73,73 +79,78 @@ namespace ObjectSizeDemo
             Console.WriteLine("Mixed2: \t" + diff);
 
             //---------------------------------------
+
+            before = GC.GetTotalMemory(true);
+            Mixed3 obj6 = new Mixed3();
+            after = GC.GetTotalMemory(true);
+            diff = after - before;
+
+            Console.WriteLine("Mixed3: \t" + diff);
+
+            //---------------------------------------
         }
 
         private static void ObjectSizes()
         {
-            int size = 100;
+            int size = 1000;
             double diff;
 
-            //long beforeObj = GC.GetTotalMemory(true);
-            //object obj = new object();
-            //long afterObj = GC.GetTotalMemory(true);
-            //diff = afterObj - beforeObj;
+            long beforeObj = GC.GetTotalMemory(true);
+            object obj = new object();
+            long afterObj = GC.GetTotalMemory(true);
+            diff = afterObj - beforeObj;
 
-            //Console.WriteLine("object: \t" + diff);
+            Console.WriteLine("obj: \t\t" + diff);
 
-            ////---------------------------------------
+            //---------------------------------------
 
-            //long beforeObjArray0 = GC.GetTotalMemory(true);
-            //object[] array0 = new object[0];
-            //long afterObjArray0 = GC.GetTotalMemory(true);
-            //diff = afterObjArray0 - beforeObjArray0;
+            long beforeObjArray0 = GC.GetTotalMemory(true);
+            object[] array0 = new object[0];
+            int[] arrayInt = new int[0];
+            long afterObjArray0 = GC.GetTotalMemory(true);
+            diff = afterObjArray0 - beforeObjArray0;
 
-            //Console.WriteLine("array0: \t" + diff);
+            Console.WriteLine("array0: \t" + diff);
 
-            ////---------------------------------------
+            //---------------------------------------
 
-            //long beforeObjArray = GC.GetTotalMemory(true);
-            //object[] arrayEmpty = new object[size];
-            //long afterObjArray = GC.GetTotalMemory(true);
-            //diff = afterObjArray - beforeObjArray;
+            long beforeObjArray = GC.GetTotalMemory(true);
+            object[] arrayEmpty = new object[size];
+            long afterObjArray = GC.GetTotalMemory(true);
+            diff = afterObjArray - beforeObjArray;
 
-            //Console.WriteLine("array: \t\t" + (diff));
-            //Console.WriteLine("array: \t\t" + (diff/size - 0.16));
+            Console.WriteLine("arrayEmpty: \t" + (diff));
+            Console.WriteLine("Per object: \t" + ((diff - 16) / size));
 
-            ////---------------------------------------
+            //---------------------------------------
+
+            //PLEASE COMMENT CODE ABOVE AS GC DOESN'T ALLOW TO MEASURE NEXT array100 PROPERLY
 
             long before = GC.GetTotalMemory(true);
-            object[] array100 = new object[size]; //416 + 100 * 12
+            object[] array100 = new object[100]; //416 + 100 * 12
 
-            for (int i = 0; i < size; i++)
+            for (int i = 0; i < 100; i++)
             {
                 array100[i] = new object();
             }
             long after = GC.GetTotalMemory(true);
             diff = after - before;
+
             Console.WriteLine("array100: \t" + diff);
-            Console.WriteLine("PerШobject: \t" + diff/size);
-
-            // Stop the GC from messing up our measurements 
-            //GC.KeepAlive(arrayEmpty);
-            //GC.KeepAlive(array0);
-            //GC.KeepAlive(obj);
-            //GC.KeepAlive(array100);
-
+            Console.WriteLine("Per object: \t" + (diff - 416) / 100);
         }
+
+        #region Simple objects just for counting their size
 
         class Empty { }
 
         class EmptyWithMethods
         {
-            private object obj;
-            private int obj1;
-            private byte bt;
             public void DO(int x)
-            {}
+            { }
 
             public virtual void DOVirtual(int x)
-            {}
+            { }
         }
 
         class OneInt32 { int x; }
@@ -149,11 +160,16 @@ namespace ObjectSizeDemo
         class Mixed1
         {
             int x;
+            byte b1;
+        }
+        
+        class Mixed2
+        {
+            int x;
             byte b1, b2, b3, b4;
-            int y, z;
         }
 
-        class Mixed2
+        class Mixed3
         {
             private delegate void del();
 
@@ -163,5 +179,6 @@ namespace ObjectSizeDemo
             int y, z;
             byte b2, b3, b4;
         }
+        #endregion
     }
 }
